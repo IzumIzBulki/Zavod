86567a05627617b5e65553a161ef1da15bf67d59
Уткин Максим Игоревич <353906@in.sohim.by>
2023-04-13 14:24:46 +0300

    Komment

--- Java/lib/JavaSemAndHomeWork-/src/main/java/lesson/OOPsem5/Work2/Main.java
+++ Java/lib/JavaSemAndHomeWork-/src/main/java/lesson/OOPsem5/Work2/Main.java
@@ -10,26 +10,28 @@ public class Main {
         Cat c = new Cat("Сиамский", "Максим", "Серый", "Мужской", Numbers.getR(2), Numbers.getR(5), 100);
         Dog dog = new Dog("Овчарка", "Арчи", "Черный", "Мужской", Numbers.getR(5), Numbers.getR(5), 100);
         Doctor d = new Doctor("Артем", "Ветеринар", "Мужской", Numbers.getR(25));
-        
+        // Списки зверей и людей
         List<Human> human = new ArrayList<>();
         List<Animal> animal = new ArrayList<>();
       
         animal.add(dog);
         animal.add(c);
         human.add(d);
-
+        //показать информацию всех зверей
         for (Animal animal2 : animal) {System.out.println(animal2);}
         
-        
+        //цикл проведения проверки и лечения всех животных
         for (int i = 0; i < animal.size(); i++) {
             Random r = new Random();
             int number = r.nextInt(1, 4);
             System.out.println(animal.get(i).getType()+" "+ "На приеме у"+" " + d.getStatus());
+            //рандомное присвоение статуса здоровья животному
             switch (number){
                 case (1):
                     System.out.println("У животного обнаружена пневмания");
+                    //метод получения урона от болезни 
                     animal.get(i).setHealth(Disease.Pneumonia((int)animal.get(i).getHealth()));
-                    
+                    //цикл лечения животного
                     while ((int) animal.get(i).getHealth()<100){
                         animal.get(i).setHealth(d.treatment((int) animal.get(i).getHealth()));
                         if ((int) animal.get(i).getHealth() > 100){
@@ -41,8 +43,9 @@ public class Main {
                     break;
                 case (2):
                     System.out.println("У животного обнаружено бешенство");
+                    //метод получения урона от болезни 
                     animal.get(i).setHealth(Disease.Rabies((int)animal.get(i).getHealth()));
-                    
+                     //цикл лечения животного
                     while ((int) animal.get(i).getHealth()<100){
                         animal.get(i).setHealth(d.treatment((int) animal.get(i).getHealth()));
                         if ((int) animal.get(i).getHealth() > 100){
